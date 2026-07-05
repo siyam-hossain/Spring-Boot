763366b9ad99cecd0a202b2296dfe137fc0eb75d
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-06 00:06:53 +0600

    Creating a spring boot project

---
+++ Spring-Boot-Part-01/001_Creating_A_Spring_Boot_Project/CreatingASpringBootProject/src/main/java/com/example/creatingaspringbootproject/CreatingASpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatingaspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingASpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingASpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Part-01/001_Creating_A_Spring_Boot_Project/CreatingASpringBootProject/src/test/java/com/example/creatingaspringbootproject/CreatingASpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creatingaspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingASpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
