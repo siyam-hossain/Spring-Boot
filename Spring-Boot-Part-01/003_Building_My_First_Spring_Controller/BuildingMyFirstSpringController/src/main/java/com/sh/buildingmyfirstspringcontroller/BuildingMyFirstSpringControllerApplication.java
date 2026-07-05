0df70d3221196ba147764234a4666969bd0108ff
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-06 00:47:18 +0600

    Building my first spring controller

---
+++ Spring-Boot-Part-01/003_Building_My_First_Spring_Controller/BuildingMyFirstSpringController/src/main/java/com/sh/buildingmyfirstspringcontroller/BuildingMyFirstSpringControllerApplication.java
@@ -0,0 +1,13 @@
+package com.sh.buildingmyfirstspringcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMyFirstSpringControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingMyFirstSpringControllerApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Part-01/003_Building_My_First_Spring_Controller/BuildingMyFirstSpringController/src/test/java/com/sh/buildingmyfirstspringcontroller/BuildingMyFirstSpringControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.buildingmyfirstspringcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingMyFirstSpringControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
