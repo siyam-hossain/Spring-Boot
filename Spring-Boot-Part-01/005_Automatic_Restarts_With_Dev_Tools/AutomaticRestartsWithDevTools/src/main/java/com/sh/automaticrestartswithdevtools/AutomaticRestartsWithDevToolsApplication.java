0e5a619bea89488b06fa6371bdede8b9a9d9179a
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-07 18:32:42 +0600

    Automatic restarts with dev tools

---
+++ Spring-Boot-Part-01/005_Automatic_Restarts_With_Dev_Tools/AutomaticRestartsWithDevTools/src/main/java/com/sh/automaticrestartswithdevtools/AutomaticRestartsWithDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.sh.automaticrestartswithdevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticRestartsWithDevToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticRestartsWithDevToolsApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Part-01/005_Automatic_Restarts_With_Dev_Tools/AutomaticRestartsWithDevTools/src/test/java/com/sh/automaticrestartswithdevtools/AutomaticRestartsWithDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.automaticrestartswithdevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticRestartsWithDevToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
