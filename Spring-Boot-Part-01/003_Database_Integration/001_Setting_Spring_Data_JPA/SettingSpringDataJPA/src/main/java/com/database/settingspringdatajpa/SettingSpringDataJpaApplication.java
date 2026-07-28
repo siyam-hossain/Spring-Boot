7e7977234b5977a71ba636f82d99c5ac0ec47cc5
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-28 22:43:37 +0600

    Setting Spring Data JPA and Create database table

---
+++ Spring-Boot-Part-01/003_Database_Integration/001_Setting_Spring_Data_JPA/SettingSpringDataJPA/src/main/java/com/database/settingspringdatajpa/SettingSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.database.settingspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Part-01/003_Database_Integration/001_Setting_Spring_Data_JPA/SettingSpringDataJPA/src/test/java/com/database/settingspringdatajpa/SettingSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.settingspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
