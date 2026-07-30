6be93595ff2841659119cc148f84f2196c1489b3
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-30 11:15:32 +0600

    simplifying code with lombok

--- Spring-Boot-Part-01/003_Database_Integration/001_Setting_Spring_Data_JPA/SettingSpringDataJPA/src/main/java/com/database/settingspringdatajpa/SettingSpringDataJpaApplication.java
+++ Spring-Boot-Part-01/003_Database_Integration/001_Setting_Spring_Data_JPA/SettingSpringDataJPA/src/main/java/com/database/settingspringdatajpa/SettingSpringDataJpaApplication.java
@@ -1,5 +1,6 @@
 package com.database.settingspringdatajpa;
 
+import com.database.settingspringdatajpa.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SettingSpringDataJpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SettingSpringDataJpaApplication.class, args);
-    }
+//        SpringApplication.run(SettingSpringDataJpaApplication.class, args);
+        var user = User.builder()
+                .username("admin")
+                .password("admin")
+                .email("admin@gmail.com")
+                .build();
 
+
+    }
 }
